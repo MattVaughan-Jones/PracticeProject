ccbf1fd0fa642dc9ff8aa64b101c9da101a563eb
Joshua You <jyou619@gmail.com>
2022-12-30 12:56:47 +1000

    Fix ES importing

--- practice-project-api/routes/calculate.ts
+++ practice-project-api/routes/calculate.ts
@@ -1,8 +1,7 @@
-const express = require("express");
-const router = express.Router();
+import { Router } from "express";
 
-router.get('/', (req: any, res: any, next: any) => {
-    res.send('this is the /calculate route');
+export const calculateRoute = Router();
+
+calculateRoute.get("/", (req, res, next) => {
+  res.send("this is the /calculate route");
 });
-
-export { router };
--- practice-project-api/server/index.ts
+++ practice-project-api/server/index.ts
@@ -1,17 +1,17 @@
 //requires
 const express = require("express");
 
-const calculate = require('./../routes/calculate.ts');
+const calculate = require("./../routes/calculate.ts");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 //use the /calculate.ts file to handle endpoints starting with /calculate
-app.use('/calculate', calculate);
+app.use("/calculate", calculate);
 
-app.get('/', (req, res) => {
-    res.send('<h1>first middleware: Hello world!</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>first middleware: Hello world!</h1>");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
