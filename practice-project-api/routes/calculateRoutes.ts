6368f94937b83eb60d251e68a2594792d0bc5484
MattVaughan-Jones <71620998+MattVaughan-Jones@users.noreply.github.com>
2022-12-31 12:10:02 +1000

    added calculate endpoint with TS interfaces and types. Not yet working

--- practice-project-api/routes/calculateRoutes.ts
+++ practice-project-api/routes/calculateRoutes.ts
@@ -1,7 +1,35 @@
 import { Router, Request, Response } from "express";
+import { calculatorOperation } from "../types";
 
 export const calculateRoute = Router();
 
 calculateRoute.get("/calculate", (req: Request, res: Response, next) => {
   res.send("this is the /calculate route");
+});
+
+calculateRoute.post("/calculate", (req: Request, res: Response, next) => {
+
+  interface Calculate {
+    firstNumber: number,
+    secondNumber: number,
+    operation: calculatorOperation,
+  };
+
+  interface Output {
+    output: Number
+  };
+
+  const input: Calculate = {
+    firstNumber: req.firstNumber,
+    secondNumber: req.firstNumber,
+    operation: req.operation,
+  };
+
+  const output = (input.firstNumber, input.operation, input.secondNumber) => {
+    //calclator code here
+    return 0;
+  };
+
+  res.send(output);
+
 });
