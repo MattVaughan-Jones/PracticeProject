f9195dcf7db4668b64af100bd3b0c336cff579e3
MattVaughan-Jones <71620998+MattVaughan-Jones@users.noreply.github.com>
2023-01-05 10:07:40 +1000

    fix API post request in express. Not calculating correctly still.

--- practice-project-api/dist/routes/calculateRoutes.js
+++ practice-project-api/dist/routes/calculateRoutes.js
@@ -3,30 +3,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.calculateRoute = void 0;
 const express_1 = require("express");
 exports.calculateRoute = (0, express_1.Router)();
-exports.calculateRoute.use(express.json());
 exports.calculateRoute.get("/calculate", (req, res, next) => {
     res.send("this is the /calculate route");
 });
 exports.calculateRoute.post("/calculate", (req, res, next) => {
     ;
-    // const input: Calculate = {
-    //   firstNumber: req.body.firstNumber,
-    //   secondNumber: req.body.firstNumber,
-    //   operation: req.body.operation,
-    // };
-    // let calculate = (firstNumber: number, operation: calculatorOperation, secondNumber: number): number => {
-    //   switch (operation) {
-    //     case '*': return firstNumber * secondNumber;
-    //     case '/': return firstNumber / secondNumber;
-    //     case '+': return firstNumber + secondNumber;
-    //     case '-': return firstNumber - secondNumber;
-    //   }
-    // };
-    // const output: Number = calculate(input.firstNumber, input.operation, input.secondNumber);
-    if (req.body) {
-        res.send(201);
-    }
-    else {
-        res.send(200);
-    }
+    const input = {
+        firstNumber: req.body.firstNumber,
+        secondNumber: req.body.firstNumber,
+        operation: req.body.operation,
+    };
+    let calculate = (firstNumber, operation, secondNumber) => {
+        switch (operation) {
+            case '*': return firstNumber * secondNumber;
+            case '/': return firstNumber / secondNumber;
+            case '+': return firstNumber + secondNumber;
+            case '-': return firstNumber - secondNumber;
+        }
+    };
+    const output = calculate(input.firstNumber, input.operation, input.secondNumber);
+    console.log(output);
+    res.end(JSON.stringify(output));
 });
--- practice-project-api/routes/calculateRoutes.ts
+++ practice-project-api/routes/calculateRoutes.ts
@@ -2,8 +2,6 @@ import { Router, Request, Response } from "express";
 
 export const calculateRoute = Router();
 
-calculateRoute.use(express.json());
-
 calculateRoute.get("/calculate", (req: Request, res: Response, next) => {
   res.send("this is the /calculate route");
 });
@@ -17,24 +15,23 @@ calculateRoute.post("/calculate", (req: Request, res: Response, next) => {
     operation: calculatorOperation,
   };
 
-  // const input: Calculate = {
-  //   firstNumber: req.body.firstNumber,
-  //   secondNumber: req.body.firstNumber,
-  //   operation: req.body.operation,
-  // };
+  const input: Calculate = {
+    firstNumber: req.body.firstNumber,
+    secondNumber: req.body.firstNumber,
+    operation: req.body.operation,
+  };
 
-  // let calculate = (firstNumber: number, operation: calculatorOperation, secondNumber: number): number => {
-  //   switch (operation) {
-  //     case '*': return firstNumber * secondNumber;
-  //     case '/': return firstNumber / secondNumber;
-  //     case '+': return firstNumber + secondNumber;
-  //     case '-': return firstNumber - secondNumber;
-  //   }
-  // };
+  let calculate = (firstNumber: number, operation: calculatorOperation, secondNumber: number): number => {
+    switch (operation) {
+      case '*': return firstNumber * secondNumber;
+      case '/': return firstNumber / secondNumber;
+      case '+': return firstNumber + secondNumber;
+      case '-': return firstNumber - secondNumber;
+    }
+  };
 
-  // const output: Number = calculate(input.firstNumber, input.operation, input.secondNumber);
-
-  if (req.body) { res.send(201); }
-  else { res.send(200);}
+  const output: Number = calculate(input.firstNumber, input.operation, input.secondNumber);
+  console.log(output);
+  res.end(JSON.stringify(output));
 
 });
