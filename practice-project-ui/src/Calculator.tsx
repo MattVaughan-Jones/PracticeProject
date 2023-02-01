6bb897cc0199557836aa10e80b90d0b487ce991e
MattVaughan-Jones <71620998+MattVaughan-Jones@users.noreply.github.com>
2023-02-01 17:57:01 +1000

    PP-7 add calculator component

--- practice-project-ui/src/Calculator.tsx
+++ practice-project-ui/src/Calculator.tsx
@@ -1,25 +1,63 @@
 import * as React from "react";
+// import axios from 'axios';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const baseURL = process.env.BASE_URL;
+
+enum Operations {
+  Add = "+",
+  Subtract = "-",
+  Multiply = "*",
+  Divide = "/"
+};
 
 function App() {
 
-  const [data, setData] = React.useState(null);
+  // const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+  // async function calculate(event: React.FormEvent<HTMLFormElement>) {
+    
+  //   event.preventDefault();
+
+  //   try {
+  //     axios.post(`${baseURL}/calculate`, {
+  //       firstValue: (event.target as HTMLFormElement).firstValue.value,
+  //       operation: (event.target as HTMLFormElement).operation.value,
+  //       secondValue: (event.target as HTMLFormElement).secondValue.value
+  //     })
+  //     .then((response) => {
+  //       setData(response.data);
+  //     });
+  //   } catch (error) {
+  //     console.log(error.response);
+  //   }
+  // }
 
   return (
     <div>
       <header>
         <p>Calculator Component</p>
-        <p>{data}</p>
+        {/* <p>Result: {data}</p> */}
       </header>
-      <body>
-      </body>
+        {/* <form onSubmit={calculate}> */}
+        <form>
+          <label>first Number : <input name="firstValue" type="number" /></label>
+          <label>Operation : 
+            <select name="operation">
+              <option value={ Operations.Multiply }>{ Operations.Multiply }</option>
+              <option value={ Operations.Divide }>{ Operations.Divide }</option>
+              <option value={ Operations.Add }>{ Operations.Add }</option>
+              <option value={ Operations.Subtract }>{ Operations.Subtract }</option>
+            </select>
+          </label>
+          <label>second Number : <input name="secondValue" type="number" /></label>
+          <button type="submit">Submit</button>
+        </form>
     </div>
   );
+
 }
 
 export default App;
