b96c44b20be03e3b6f12773fb0a7bf5dc0cf41c8
MattVaughan-Jones <71620998+MattVaughan-Jones@users.noreply.github.com>
2023-02-14 21:32:58 +1000

    deleted theme.tx file because unused

--- practice-project-ui/src/index.tsx
+++ practice-project-ui/src/index.tsx
@@ -1,34 +1,13 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import * as ReactDOM from 'react-dom/client';
 import Calculator from './Calculator';
 import * as React from 'react';
 
-const theme = createTheme({
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: `
-        h1 {
-          color: grey;
-        }
-      `,
-    },
-  },
-});
-
-export default function OverrideCssBaseline() {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <h1>Grey h1 element</h1>
-    </ThemeProvider>
-  );
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <CssBaseline />
-    <Calculator />
-  </React.StrictMode>
+    <React.StrictMode>
+      <CssBaseline />
+      <Calculator />
+    </React.StrictMode>
 );
