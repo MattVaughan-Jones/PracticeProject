fb68a072575f9f41f1abf4d6de7a603d652fb5bd
MattVaughan-Jones <71620998+MattVaughan-Jones@users.noreply.github.com>
2023-02-15 19:48:42 +1000

    Pp 17 (#10)
    * implemented MaterialUI
    
    * update form field and button appearance
    
    * added basic non-responsive layout
    
    * deleted theme.tx file because unused
    
    * removed redundant theme import from index.tsx
    
    * addressed PR comments: combined imports into single statements

--- practice-project-ui/src/Calculator.tsx
+++ practice-project-ui/src/Calculator.tsx
@@ -1,9 +1,6 @@
-import * as React from "react";
+import { FormControl, TextField, Container, MenuItem, Button, Grid, Box } from '@mui/material';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import dotenv from 'dotenv';
-
 
 const baseURL = process.env.REACT_APP_BASE_URL;
 
@@ -53,31 +50,96 @@ function Calculator() {
       setInputs(values => ({...values, [name]: value}))
     }
   
-    return (
-      <>
+  return (
+    <>
+      <head>
+        <meta name="viewport" content="initial-scale=1, width=device-width"/>
+      </head>
+      <Container>
         <header>
-          <p>Calculator Component</p>
-          <p>Result: {result}</p>
+          <h1>Calculator</h1>
         </header>
+        <Box
+          sx={{
+            my: 3,
+            width: '469px',
+            bgcolor: 'grey.100',
+            color: 'grey.800',
+            border: '1px solid',
+            borderRadius: 2,
+            fontSize: '2rem',
+            fontWeight: '700',
+            textAlign: 'center',
+          }}
+        >
+          <p>{result}</p>
+        </Box>
         <form onSubmit={calculate}>
-          <label>First Value: 
-            <input type="number" name="firstValue" value={inputs.firstValue} onInput={handleChange}/>
-          </label>
-          <label>Operation : 
-            <select name="operation">
-              <option value={ Operation.Multiply }>{ Operation.Multiply }</option>
-              <option value={ Operation.Divide }>{ Operation.Divide }</option>
-              <option value={ Operation.Add }>{ Operation.Add }</option>
-              <option value={ Operation.Subtract }>{ Operation.Subtract }</option>
-            </select>
-          </label>
-          <label>Second Value: 
-            <input type="number" name="secondValue" value={inputs.secondValue} onInput={handleChange}/>
-          </label>
-          <button type="submit">Submit</button>
+          <Grid 
+            container
+            direction={{ xs: 'column', sm: 'row' }}
+            rowSpacing={{xs: 1, sm: 0}}
+            justifyContent={{xs:'center', sm:'flex-start'}}
+            alignItems='center'
+          >
+            <Grid item pr={'15px'}>
+              <FormControl>
+                <TextField 
+                  label="first number" 
+                  type="number" 
+                  name="firstValue" 
+                  value={inputs.firstValue} 
+                  onInput={handleChange}
+                  style={{ width: 120}}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item pr={'15px'}>
+              <FormControl>
+                <TextField
+                  select
+                  label="Operation"
+                  name="operation"
+                  defaultValue={Operation.Add}
+                  style={{ width: 120}}
+                >
+                  <MenuItem key={Operation.Multiply} value={Operation.Multiply}>
+                    {Operation.Multiply}
+                  </MenuItem>
+                  <MenuItem key={Operation.Divide} value={Operation.Divide}>
+                    {Operation.Divide}
+                  </MenuItem>
+                  <MenuItem key={Operation.Add} value={Operation.Add}>
+                    {Operation.Add}
+                  </MenuItem>
+                  <MenuItem key={Operation.Subtract} value={Operation.Subtract}>
+                    {Operation.Subtract}
+                  </MenuItem>
+                </TextField>
+              </FormControl>
+            </Grid>
+            <Grid item pr={'15px'}>
+              <FormControl>
+                <TextField 
+                  label="second number" 
+                  type="number" 
+                  name="secondValue" 
+                  value={inputs.secondValue} 
+                  onInput={handleChange}
+                  style={{ width: 120}}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item>
+              <FormControl>
+                <Button sx={{fontSize: 25}} variant="outlined" type="submit">=</Button>
+              </FormControl>
+            </Grid>
+          </Grid>
         </form>
-      </>
-    )
+      </Container>
+    </>
+  )
 }
 
 export default Calculator;
--- practice-project-ui/src/index.tsx
+++ practice-project-ui/src/index.tsx
@@ -1,10 +1,12 @@
-import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
 import * as ReactDOM from 'react-dom/client';
 import Calculator from './Calculator';
+import * as React from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Calculator />
-  </React.StrictMode>
+    <React.StrictMode>
+      <CssBaseline />
+      <Calculator />
+    </React.StrictMode>
 );
